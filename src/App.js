aec86dbf75373936a65b7c852fbc3a051e4c5a87
RLoo <jonathanfaren@gmail.com>
2022-01-06 17:24:04 +0700

    wlcome and landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import Intro from './pages/intro';
-import Fullpage from './pages/fullpage';
-import React from 'react'
+import "./App.css";
+import Intro from "./pages/intro";
+import Fullpage from "./pages/fullpage";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  
   return (
     <>
-    <Router>
+      <Router>
         <Switch>
-          <Route exact path="/" component={Intro} />
+          <Route exact path="/" component={Fullpage} />
           <Route exact path="/welcome" component={Intro} />
         </Switch>
       </Router>
-      </>
+    </>
   );
 }
 
