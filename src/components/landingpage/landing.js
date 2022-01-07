a05e872fe61b2c39eb60102f0e803349191ba9a4
RLoo <jonathanfaren@gmail.com>
2022-01-07 15:27:16 +0700

    landing animation

--- src/components/landingpage/landing.js
+++ src/components/landingpage/landing.js
@@ -5,11 +5,36 @@ import iconImg2 from "../.././assets/images/iconMascot2.svg";
 import iconImg3 from "../.././assets/images/iconMascot3.svg";
 import iconLogo from "../.././assets/images/iconLogo.svg";
 import iconInsta from "../.././assets/images/iconInsta.svg";
+import { useSpring, animated } from "react-spring";
 
-const landing = () => {
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const trans1 = (x, y) => `translate3d(${x / 10 + 100}px,${y / 100 - 10}px,0)`;
+const trans2 = (x, y) => `translate3d(${x / 300 - 0}px,${y / 10+10}px,0)`;
+const trans3 = (x, y) => `translate3d(${x / 1000 + 0}px,${y / 10+10}px,0)`;
+
+const Landing = () => {
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
   return (
     <>
-      <div className="background">
+      <div
+        className="background"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+      >
+        <animated.div
+          class="card42"
+          style={{ transform: props.xy.interpolate(trans1) }}
+        />
+        <animated.div
+          class="card43"
+          style={{ transform: props.xy.interpolate(trans2) }}
+        />
+        <animated.div
+          class="card44"
+          style={{ transform: props.xy.interpolate(trans3) }}
+        />
         <div className="bgContainer">
           <div className="bgContainerText">
             <div>
@@ -35,13 +60,10 @@ const landing = () => {
               <span className="footerText">afairui</span>
             </div>
           </div>
-          <img src={iconImg1} alt="icon1" className="topleftIcon" />
-          <img src={iconImg3} alt="icon3" className="bottomleftIcon" />
-          <img src={iconImg2} alt="icon2" className="bottomrightIcon" />
         </div>
       </div>
     </>
   );
 };
 
-export default landing;
+export default Landing;
