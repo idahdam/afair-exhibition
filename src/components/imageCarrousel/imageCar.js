d6dd87ed0720bf1661d68c6db1e639988c7c4feb
RLoo <jonathanfaren@gmail.com>
2022-01-18 23:07:00 +0700

    add modal

--- src/components/imageCarrousel/imageCar.js
+++ src/components/imageCarrousel/imageCar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Carousel from "react-spring-3d-carousel";
 import "./imageCar.css";
 import { v4 as uuidv4 } from "uuid";
@@ -12,20 +12,73 @@ import test6img from "../../assets/images/test/6.png";
 import test7img from "../../assets/images/test/7.png";
 import arrow from "../../assets/images/mainPage/arrowDark.svg";
 import info from "../../assets/images/mainPage/iconInfo.svg";
-export default class Example extends Component {
-  state = {
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { makeStyles } from "@material-ui/core/styles";
+import PopUp from "../popUP/popUp";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+function ImgCar() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const [state, setState] = useState({
     goToSlide: 0,
     offsetRadius: 2,
     showNavigation: true,
     config: config.gentle,
-  };
+  });
 
-  slides = [
+  let slides = [
     {
       key: uuidv4(),
       content: (
         <div className="imgCarContainer">
-          <img src={test1img} alt="1" className="imageContent" />
+          <img
+            src={test1img}
+            alt="1"
+            className="imageContent"
+            onClick={handleOpen}
+          />
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.transparent}>
+                <PopUp />
+              </div>
+            </Fade>
+          </Modal>
           <div className="imgCarText">
             <span className="imgCarTitle">Xiao ting</span>
             <br />
@@ -112,62 +165,64 @@ export default class Example extends Component {
         </div>
       ),
     },
-  ];
+  ].map((slide, index) => {
+    return { ...slide, onClick: () => setState({ goToSlide: index }) };
+  });
 
-  render() {
-    return (
-      <div className="carrouselBackground">
-        <div className="carrouselContainer">
-          <div className="carrouselSide">
-            {" "}
-            <button
-              onClick={() => {
-                this.setState({ goToSlide: this.state.goToSlide - 1 });
-              }}
-              className="buttonImgCar"
-            >
-              <img src={arrow} alt="arrow" className="iconArrowLeft" />
-            </button>
+  return (
+    <div className="carrouselBackground">
+      <div className="carrouselContainer">
+        <div className="carrouselSide">
+          {" "}
+          <button
+            onClick={() => {
+              setState({ goToSlide: state.goToSlide - 1 });
+            }}
+            className="buttonImgCar"
+          >
+            <img src={arrow} alt="arrow" className="iconArrowLeft" />
+          </button>
+        </div>
+        <div className="carrouselMid">
+          <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
+            <Carousel
+              slides={slides}
+              goToSlide={state.goToSlide}
+              offsetRadius={state.offsetRadius}
+              animationConfig={state.config}
+            />
           </div>
-          <div className="carrouselMid">
-            <div style={{ width: "80%", height: "500px", margin: "0 auto" }}>
-              <Carousel
-                slides={this.slides}
-                goToSlide={this.state.goToSlide}
-                offsetRadius={this.state.offsetRadius}
-                animationConfig={this.state.config}
-              />
+        </div>
+        <div className="carrouselSide">
+          {" "}
+          <button
+            onClick={() => {
+              setState({ goToSlide: state.goToSlide + 1 });
+            }}
+            className="buttonImgCar"
+          >
+            <img src={arrow} alt="arrow" className="iconArrowRight" />
+          </button>
+        </div>{" "}
+        <div className="footerCar">
+          <div className="footerCarLeft">
+            <img src={info} alt="info" className="infoCarStyle" />
+            <div class="infoCarText">
+              <p>Into The Wayout Future</p>
+              <p>
+                Unseen changes that is only felt by ourselves. They often relate
+                to each and every individual's emotions
+              </p>
             </div>
           </div>
-          <div className="carrouselSide">
-            {" "}
-            <button
-              onClick={() => {
-                this.setState({ goToSlide: this.state.goToSlide + 1 });
-              }}
-              className="buttonImgCar"
-            >
-              <img src={arrow} alt="arrow" className="iconArrowRight" />
-            </button>
-          </div>{" "}
-          <div className="footerCar">
-            <div className="footerCarLeft">
-              <img src={info} alt="info" className="infoCarStyle" />
-              <div class="infoCarText">
-                <p>Into The Wayout Future</p>
-                <p>
-                  Unseen changes that is only felt by ourselves. They often
-                  relate to each and every individual's emotions
-                </p>
-              </div>
-            </div>
-            <div className="footerCarRight">
-              <span className="iconCarFloat">To The Exit</span>
-              <img src={arrow} alt="arrow" className="icoCarStyle" />
-            </div>
+          <div className="footerRight exitButton">
+            <span className="iconFloat">To The Exit</span>
+            <img src={arrow} alt="arrow" className="icoStyle" />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default ImgCar;
--- src/components/popUP/popUp.js
+++ src/components/popUP/popUp.js
@@ -1,5 +1,4 @@
 import React from "react";
-import arrow from "../../assets/images/mainPage/arrowDark.svg";
 import "./popUp.css";
 import popUpImg1 from "../../assets/images/popUp/popUpImage1.svg";
 import popUpImg2 from "../../assets/images/popUp/popUpImage2.svg";
@@ -8,24 +7,45 @@ import popUpImg3 from "../../assets/images/popUp/popUpImage3.svg";
 const popUp = () => {
   return (
     <>
-      <div className="popUpBg">
-        <div className="popUpContainerWrapper">
-          <div className="popUpContainer">
-            <div className="popUpWrapper">
-              <div className="popUpContainerSide">
-                <img src={arrow} alt="arrow" className="icoStyle" />
+    <div className="popUpBg">
+      <div className="popUpContainerWrapper">
+        <div className="popUpContainer">
+          <div className="popUpWrapper">
+            <div className="popUpContainerMid">
+              <div className="popUpLeft">
+                <img src={popUpImg1} alt="Mainphoto" className="mainImgStyle" />
               </div>
-              <div className="popUpContainerMid">
-                <div className="popUpLeft"> 
-                <img src={popUpImg1} alt="Mainphoto" className="mainImgStyle" /></div>
-                <div className="popUpRight">sadasdas</div>
-              </div>
-              <div className="popUpContainerSide">
-                <img src={arrow} alt="arrow" className="icoStyle" />
+              <div className="popUpRight">
+                <div className="popUpTextContainer">
+                  <div className="popUpTextTitle">
+                    The Lip
+                    <br />
+                    Salman Bob
+                  </div>
+                  <div className="popUpTextContent">
+                    <img src={popUpImg2} alt="Mainphoto" className="ContentImg"/>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nulla nec ultricies urna, quis tempus enim. Nullam
+                    ullamcorper metus non orci condimentum, ac dapibus purus
+                    commodo. Fusce fringilla orci auctor, feugiat tellus vel,
+                    lacinia nunc. Fusce vel laoreet nulla. Quisque rhoncus
+                    sodales risus vel laoreet. Pellentesque imperdiet metus at
+                    est luctus, vitae cursus mauris elementum. Curabitur lectus
+                    magna, rhoncus ac imperdiet et, consequat quis libero. Nam
+                    dictum, dui id laoreet porta, est libero pretium erat, non
+                    cursus augue felis vitae massa. Donec vel commodo dolor, vel
+                    interdum diam. Sed mattis, mauris vel iaculis rhoncus, velit
+                    magna suscipit odio, ultrices fermentum turpis urna vitae
+                    metus. Mauris eleifend metus ac purus congue dictum. Quisque
+                    vel sapien auctor, fermentum sem ut, viverra velit.
+                    <img src={popUpImg3} alt="Mainphoto" className="ContentImg"/>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
