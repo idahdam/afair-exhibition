27fcf57041fe9078dab7ab9ce49c1d619aa2c0d8
idahdam <nasshitell@gmail.com>
2022-01-19 22:50:36 +0700

    ENV

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = process.env.REACT_APP_API_URL;
-const BASE_URL = "http://156.67.214.132/api/api/";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 async function http(method, endpoint, body = null, data) {
   const headers = {
