492fec902fad9887589cbff2907cb229bdd6e433
RLoo <jonathanfaren@gmail.com>
2022-01-20 11:08:23 +0700

    goodbye done

--- src/pages/GoodBye/goodBye3.js
+++ src/pages/GoodBye/goodBye3.js
@@ -1,38 +1,60 @@
-import React from 'react';
-
+import React from "react";
+import "./index.css";
+import logoBanyak from "../../assets/images/logoBanyak.png";
+import logoBE from "../../assets/images/logoBE.png";
+import logoHino from "../../assets/images/logoHino.png";
+import logoMMAR from "../../assets/images/logoMMAR.png";
 const goodBye3 = () => {
-return (
+  return (
     <>
       <div className="section2bg">
         <div className="section2wrapper">
           <div className="section2Left">
-            <span className="section2LeftTitle">
-            Thank You To Our Supporters</span>
-            </div>
+            <span className="section2LeftTitle" style={{ fontSize: "40px" }}>
+              Thank You <br /> To Our <br /> Supporters
+            </span>
+          </div>
           <div className="section2Right">
             {" "}
             <div className="SectionRighRow">
               <div className="SectionRightColumnLeft">
                 <div className="contentContainer">
-                  <div className="contentTitle">Project Officer </div>
-                  <div className="contentMember">M. Aqil Ersa Putra</div>
-                </div>
-              </div>
-
-              <div className="SectionRightColumnRight">
-                <div className="contentContainer">
-                  <div className="contentTitle">Curator</div>
-                  <div className="contentMember">Prof. Yandi Andri Yatmo</div>
-                  <div className="contentMember">Kristanti Paramita</div>
+                  <div className="contentTitle">Media Partners </div>
+                  <div className="contentImg">
+                    <img src={logoBanyak} alt="logoBnayak" />
+                  </div>
                 </div>
               </div>
             </div>
-
+            <div className="SectionRighRow">
+              <div className="SectionRightColumnLeft" style={{ width: "100%" }}>
+                <div className="contentContainer">
+                  <div className="contentTitle">Sponsors</div>
+                  <div className="contentImg">
+                    <img
+                      src={logoBE}
+                      alt="logoBnayak"
+                      className="logoGoodStyle"
+                    />
+                    <img
+                      src={logoMMAR}
+                      alt="logoBnayak"
+                      className="logoGoodStyle"
+                    />
+                    <img
+                      src={logoHino}
+                      alt="logoBnayak"
+                      className="logoGoodStyle"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
-  );;
+  );
 };
 
 export default goodBye3;
